84cdb9955404bb9fd102506e3e92d0d59b67af97
Eugene Butusov <EvgenyButusov@gmail.com>
2016-12-30 13:51:33 +0300

    Initial Commit

---
+++ CSVTool/CSVTool.h
@@ -0,0 +1,19 @@
+//
+//  CSVTool.h
+//  CSVTool
+//
+//  Created by Eugene Butusov on 30/12/2016.
+//  Copyright © 2016 VedideV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSVTool.
+FOUNDATION_EXPORT double CSVToolVersionNumber;
+
+//! Project version string for CSVTool.
+FOUNDATION_EXPORT const unsigned char CSVToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSVTool/PublicHeader.h>
+
+
---
+++ CSVToolTests/CSVToolTests.m
@@ -0,0 +1,39 @@
+//
+//  CSVToolTests.m
+//  CSVToolTests
+//
+//  Created by Eugene Butusov on 30/12/2016.
+//  Copyright © 2016 VedideV. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CSVToolTests : XCTestCase
+
+@end
+
+@implementation CSVToolTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
